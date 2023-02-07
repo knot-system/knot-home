a648fc403695f870825754477f45996c3bb7e652
maxhaesslein <git@maxhaesslein.de>
2023-02-07 12:00:39 +0100

    microsub: allow following of new feeds

--- system/site/microsub.php
+++ system/site/microsub.php
@@ -29,12 +29,40 @@ if( ! isset($_SESSION['scope']) || ! in_array( 'read', explode( ' ', $_SESSION['
 
 // NOTE: see https://aperture.p3k.io/docs for aperture docs
 
+// needs 'read' scope (?)
+$channels = np_ms_api_get( 'channels' );
+if( $channels && isset($channels->channels) && count($channels->channels) ) {
+	// overview: list channels
+	?>
+	<h2>Channels</h3>
+	<ul>
+	<?php
+	foreach( $channels->channels as $channel ) {
+
+		if( isset($_GET['channel']) && $channel->uid == $_GET['channel'] ) {
+			?>
+			<li><?= $channel->name ?></li>
+			<?php
+		} else {
+			?>
+			<li><a href="<?= url('microsub') ?>?channel=<?= $channel->uid; ?>"><?= $channel->name.' ['.$channel->unread.' unread]'; ?></a></li>
+			<?php
+		}
+	}
+	?>
+	</ul>
+	<?php
+} else {
+	echo '<strong>CHANNELS ERROR:</strong>';
+	echo '<code><pre>';
+	var_dump($channels);
+	echo '</pre></code>';
+}
+
 
 if( isset($_GET['channel']) ) {
+
 	// content of channel
-
-	echo '<p><a href="'.url('microsub').'">&laquo; All Channels</a></p>';
-
 
 	// needs 'follow' scope
 	$feeds = np_ms_api_get( 'follow', array( 'channel' => $_GET['channel'] ) );
@@ -47,11 +75,18 @@ if( isset($_GET['channel']) ) {
 		foreach( $feeds->items as $item ) {
 			?>
 			<li>
-				<a href="<?= $item->url ?>" target="_blank" rel="noopener"><?= $item->url ?></a>
+				<?php
+				if( ! empty($item->name) ) echo $item->name.' (';
+				echo $item->url;
+				if( ! empty($item->name) ) echo ')';
+				?>
 			</li>
 			<?php
 		}
 		?>
+			<li>
+				<a href="<?= url('microsub/?channel='.$_GET['channel'].'&action=add', false ) ?>">add a new feed to this channel</a>
+			</li>
 		</ul>
 		<?php
 	} else {
@@ -61,76 +96,94 @@ if( isset($_GET['channel']) ) {
 		echo '</pre></code>';
 	}
 
+	if( isset($_GET['action']) && $_GET['action'] == 'add' ) {
+		// add new feed
 
-	// needs 'read' scope (?)
-	$items = np_ms_api_get( 'timeline', array( 'channel' => $_GET['channel'] ) );
-	if( $items && isset($items->items) && count($items->items) ) {
-		?>
-		<hr>
-		<h2>Posts</h2>
-		<ul class="posts">
-		<?php
-		foreach( $items->items as $item ) {
+		echo '<hr>';
 
-			$date = new DateTimeImmutable($item->published);
+		echo '<p><a href="'.url('microsub?channel='.$_GET['channel'], false).'">&laquo; abort adding a new feed</a></p>';
 
-			$datetime_format = $sekretaer->config->get('datetime_format');
-			$datetime = $date->format( $datetime_format );
+		if( isset($_POST['url']) ) {
 
+			$search_url = $_POST['url'];
+
+			// TODO: search for feed urls: https://indieweb.org/Microsub-spec#Search
+			//$sources = np_ms_api_get( 'search', array( 'query' => $search_url ) );
+
+			// TODO: add feed validation (currently, everything gets added, even if its not a valid feed)
+
+			// follow feed - https://indieweb.org/Microsub-spec#Following
+			$response = np_ms_api_post( 'follow', [
+				'channel' => $_GET['channel'],
+				'url' => $search_url
+			] );
+
+			echo '<p><strong>server response:</strong></p>';
+			echo '<pre>';
+			var_dump($response);
+			echo '</pre>';
+
+			echo '<a href="'.url('microsub?channel='.$_GET['channel'].'&refresh=true', false).'">&raquo; back to the channel overview</a>';
+
+		} else {
 			?>
-			<li>
-				<?php
-				if( isset($_GET['debug']) ) {
-					?>
-					<pre><?php var_dump($item); ?></pre>
-					<?php
-				}
+			<form method="POST" action="<?= url('microsub?channel='.$_GET['channel'].'&action=add', false ) ?>">
+				<p><strong>currently, the url does not get validated. only add valid json, rss or atom feeds.</strong></p>
+				<label style="display: inline-block;">Feed URL (json, rss, atom, ...): <input type="url" name="url" placeholder="https://www.example.com/feed/rss" style="min-width:400px;"></label>
+				<button>add feed</button>
+			</form>
+			<?php
+		}
+
+	} else {
+		// list posts
+
+		$items_args = array( 'channel' => $_GET['channel'] );
+		$items = np_ms_api_get( 'timeline', $items_args );
+		if( $items && isset($items->items) && count($items->items) ) {
+			?>
+			<hr>
+			<h2>Posts</h2>
+			<ul class="posts">
+			<?php
+			foreach( $items->items as $item ) {
+
+				$date = new DateTimeImmutable($item->published);
+
+				$datetime_format = $sekretaer->config->get('datetime_format');
+				$datetime = $date->format( $datetime_format );
+
 				?>
-				<?php if( ! empty( $item->photo[0] ) ) echo '<img src="'.$item->photo[0].'"><br>'; ?>
-				<h3><?= $item->name ?></h3>
-				<p>
-					<?php if( ! empty($item->content->html) ) echo $item->content->html; ?>
-				</p>
-				<p><small><a href="<?= $item->author->url ?>" target="_blank" rel="noopener"><?= $item->author->name ?></a>, <?= $datetime ?></small></p>
-				<p><a class="button" href="<?= $item->url ?>" target="_blank" rel="noopener">read full post <sup>🡥</sup></a> <a class="button" href="<?= url('micropub') ?>?content=<?= urlencode($item->url) ?>">share this post</a></p>
-			</li>
-			<?php
-		}
-		?>
-		</ul>
-		<?php
-	} else {
-		echo '<strong>POSTS ERROR:</strong>';
-		echo '<code><pre>';
-		var_dump($items);
-		echo '</pre></code>';
-	}
-
-
-} else {
-	// overview: list channels
-
-	// needs 'read' scope (?)
-	$channels = np_ms_api_get( 'channels' );
-	if( $channels && isset($channels->channels) && count($channels->channels) ) {
-		?>
-		<h2>Channels</h3>
-		<ul>
-		<?php
-		foreach( $channels->channels as $channel ) {
+				<li>
+					<?php
+					if( isset($_GET['debug']) ) {
+						?>
+						<pre><?php var_dump($item); ?></pre>
+						<?php
+					}
+					?>
+					<?php if( ! empty( $item->photo[0] ) ) echo '<img src="'.$item->photo[0].'"><br>'; ?>
+					<h3><?= $item->name ?></h3>
+					<p>
+						<?php if( ! empty($item->content->html) ) echo $item->content->html; ?>
+					</p>
+					<p><small><a href="<?= $item->author->url ?>" target="_blank" rel="noopener"><?= $item->author->name ?></a>, <?= $datetime ?></small></p>
+					<p><a class="button" href="<?= $item->url ?>" target="_blank" rel="noopener">read full post <sup>🡥</sup></a> <a class="button" href="<?= url('micropub') ?>?content=<?= urlencode($item->url) ?>">share this post</a></p>
+				</li>
+				<?php
+			}
 			?>
-			<li><a href="<?= url('microsub') ?>?channel=<?= $channel->uid; ?>"><?= $channel->name.' ('.$channel->uid.') ['.$channel->unread.' unread]'; ?></a></li>
+			</ul>
 			<?php
+		} else {
+			echo '<strong>POSTS ERROR:</strong>';
+			echo '<code><pre>';
+			var_dump($items);
+			echo '</pre></code>';
 		}
-		?>
-		</ul>
-		<?php
-	} else {
-		echo '<strong>CHANNELS ERROR:</strong>';
-		echo '<code><pre>';
-		var_dump($channels);
-		echo '</pre></code>';
+
 	}
+	
 }
 
 
@@ -147,7 +200,6 @@ function np_ms_api_get( $action, $args = array() ) {
 			$url .= '&'.$key.'='.$value; // TODO: sanitize
 		}
 	}
-
 
 	$cache = new Cache( 'microsub', $url, false, 60*3 ); // cache for 3 minutes
 
@@ -175,6 +227,43 @@ function np_ms_api_get( $action, $args = array() ) {
 	$cache->add_data( json_encode($json) );
 
 	return $json;
+}
+
+
+function np_ms_api_post( $action, $args = array() ) {
+
+	$api_url = $_SESSION['microsub_endpoint'];
+
+	$authorization = 'Authorization: Bearer '.$_SESSION['access_token'];
+
+	$url = $api_url.'?action='.$action;
+
+	$post_args = array();
+	if( count($args) ) {
+		foreach( $args as $key => $value ) {
+			$post_args[] = $key.'='.$value; // TODO: sanitize
+		}
+	}
+	$post_args = implode('&', $post_args);
+
+	if( isset($_REQUEST['debug']) ) {
+		echo '<p><strong>API request to:</strong> '.$url.'</p>';
+		echo '<pre><code>';
+	}
+
+	$ch = curl_init( $url );
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, array($authorization) );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt( $ch, CURLOPT_POST, 1 );
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, $post_args );
+	$server_output = curl_exec($ch);
+	curl_close($ch);
+
+	if( isset($_REQUEST['debug']) ) {
+		echo '</code></pre>';
+	}
+
+	return $server_output;
 }
 
 
